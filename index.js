ddd80bf0738b48ba7a8d97551ec65ad08b84ebb4
Jezero20 <68257007+Jezero20@users.noreply.github.com>
2020-07-14 23:10:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -147,6 +147,10 @@ client.on('message', message => {
 	{
 		message.channel.send('https://cdn.betterttv.net/emote/5b9ece3bee018c1569cb8608/3x');
 	}
+	if (message.content.indexOf('Kappa')!=-1)
+	{
+		message.channel.send('https://cdn.betterttv.net/emote/5d3e0e1236c0c02aeb6d2b30/3x');
+	}
 	if (message.content.indexOf('Clap')!=-1)
 	{
 		message.channel.send('https://cdn.betterttv.net/emote/55b6f480e66682f576dd94f5/3x');
